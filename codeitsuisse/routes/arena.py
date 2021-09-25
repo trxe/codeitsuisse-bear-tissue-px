7527e1e669ce6ec72bb22a5b521e5c2ed4bee0f7
trxe <peixianwong@gmail.com>
2021-09-25 15:46:50 +0800

    update arena

--- codeitsuisse/routes/arena.py
+++ codeitsuisse/routes/arena.py
@@ -1,8 +1,10 @@
 import logging
 import json
 import requests
+import queue
+import time
 
-from flask import request, jsonify
+from flask import request, jsonify, Response, render_template, Flask
 
 from codeitsuisse import app
 
@@ -13,12 +15,22 @@ def evaluateArena():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     battleId = data.get("battleId")
-    print(battleId)
-    #print(get_data(battleId))
+    URL = "https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + battleId 
+    while True:
+        requests.get(url=URL)
+        time.sleep(1)
+        pass
 
-    logging.info("My result :{}".format(battleId))
-    return json.dumps(battleId)
+def get_message():
+    '''this could be any function that blocks until data is ready'''
+    time.sleep(1.0)
+    s = time.ctime(time.time())
+    return s
 
-def get_data(battleId):
-    return requests.get("https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + battleId).content
-
+@app.route('/stream')
+def stream():
+    def eventStream():
+        while True:
+            # wait for source data to be available, then push it
+            yield 'data: {}\n\n'.format(get_message())
+    return Response(eventStream(), mimetype="text/event-stream")
