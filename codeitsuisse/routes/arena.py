d691a02dce0d7a29ec28756692f2cb5ca8b628a3
trxe <peixianwong@gmail.com>
2021-09-25 16:01:33 +0800

    update arena

--- codeitsuisse/routes/arena.py
+++ codeitsuisse/routes/arena.py
@@ -18,17 +18,26 @@ def evaluateArena():
     print(battleId)
     URL = "https://cis2021-arena.herokuapp.com/tic-tac-toe/start/" + battleId 
     print(URL)
+    test = requests.get(url=URL).json()
+    print(test)
+    generator = stream(URL)
+    for msg in generator:
+        print(msg)
+        time.sleep(1.0)
 
-def get_message():
+    return test
+
+def get_message(URL):
     '''this could be any function that blocks until data is ready'''
+    requests.get(url=URL)
     time.sleep(1.0)
     s = time.ctime(time.time())
     return s
 
 @app.route('/stream')
-def stream():
+def stream(URL):
     def eventStream():
         while True:
             # wait for source data to be available, then push it
-            yield 'data: {}\n\n'.format(get_message())
+            yield 'data: {}\n\n'.format(get_message(URL))
     return Response(eventStream(), mimetype="text/event-stream")
