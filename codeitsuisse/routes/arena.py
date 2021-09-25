2113ba0e5872965799f529f0a31b89e62e384415
trxe <peixianwong@gmail.com>
2021-09-25 17:59:25 +0800

    attempt 2 to post: checking status code

--- codeitsuisse/routes/arena.py
+++ codeitsuisse/routes/arena.py
@@ -11,10 +11,18 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 payload = { "action": "putSymbol", "position": "SE" }
 invalid = { "action": "(╯°□°)╯︵ ┻━┻" }
-initial = None
-myId = "O"
 
-grid = {
+def isValid(grid, loc, player):
+    if not loc in grid:
+        return False
+    if grid[loc] != '-':
+        return False
+    return True
+
+@app.route('/tic-tac-toe', methods=['POST'])
+def evaluateArena():
+    global payload, invalid
+    grid = {
     'NW': '-', 
     'N': '-', 
     'NE': '-', 
@@ -24,17 +32,10 @@ grid = {
     'SW': '-', 
     'S': '-', 
     'SE': '-', 
-}
+    }
+    initial = None
+    myId = "O"
 
-def isValid(loc, player):
-    if not loc in grid:
-        return False
-    if grid[loc] != '-':
-        return False
-    return True
-
-@app.route('/tic-tac-toe', methods=['POST'])
-def evaluateArena():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     battleId = data.get("battleId")
@@ -43,23 +44,32 @@ def evaluateArena():
     messages = SSEClient(URL)
     for msg in messages:
         if initial == None:
-            initial = json.loads(msg)
+            initial = json.loads(msg.__str__())
+            print(initial)
             myId = initial['youAre']
             print(myId)
+            if (myId == 'O'):
+                result = requests.post(URL, data=payload, timeout=3.0)
+                if result.status_code == requests.codes.ok: 
+                    print("ok liao")
+                else:
+                    print(result.status_code, result.reason)
+                    return json.dumps(None)
+
         else:
-            action = json.loads(msg)
+            action = json.loads(msg.__str__())
             if (action['action'] != 'putSymbol'):
-                continue
-            print(msg)
-            loc = msg['position']
-            player = msg['player']
-            if isValid(loc, player):
+                return json.dumps(None)
+            print(action)
+            loc = action['position']
+            player = action['player']
+            if isValid(grid, loc, player):
                 print("valid")
                 grid[loc] = player
-                requests.post(URL, data=payload)
+                requests.post(URL, data=payload, timeout=3.0)
             else:
                 print("invalid")
-                requests.post(URL, data=invalid)
+                requests.post(URL, data=invalid, timeout=3.0)
         time.sleep(1.0)
 
-    return json.dumps(invalid)
+    return json.dumps(None)
