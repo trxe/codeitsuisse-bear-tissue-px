3913749f508710ebde1f598781f775b8ba079f77
trxe <peixianwong@gmail.com>
2021-09-25 20:16:28 +0800

    write test cases

--- codeitsuisse/routes/arena.py
+++ codeitsuisse/routes/arena.py
@@ -13,7 +13,7 @@ payload = { "action": "putSymbol", "position": "SE" }
 invalid = { "action": "(╯°□°)╯︵ ┻━┻" }
 end_game = json.dumps(None)
 
-def is_valid(grid, loc, player):
+def is_valid(grid, loc):
     if not loc in grid:
         return False
     if grid[loc] != '-':
@@ -35,8 +35,25 @@ def modify_position(payload, grid):
             break
 
 def print_grid(grid):
-    output = '{} {} {}\n{} {} {}\n{} {} {}'.format(grid['NW'], grid['W'], grid['NE'], grid['W'], grid['C'], grid['E'], grid['SW'], grid['S'], grid['SE'])
+    output = '{} {} {}\n{} {} {}\n{} {} {}'.format(grid['NW'], grid['N'], grid['NE'], grid['W'], grid['C'], grid['E'], grid['SW'], grid['S'], grid['SE'])
     print(output)
+
+def tests(loc, me, you):
+    print("you put", loc)
+    pl = { "action": "putSymbol", "position": "SE" }
+    mgrid = { 'NW': 'X', 'N': '-', 'NE': '-', 
+    'W': '-', 'C': '-', 'E': '-', 
+    'SW': '-', 'S': '-', 'SE': 'O' }
+    if is_valid(mgrid, loc):
+        mgrid[loc] = you
+        if not is_valid(mgrid, pl['position']):
+            modify_position(pl, mgrid)
+        print("i play:", pl)
+        print(mgrid[pl['position']])
+    print_grid(mgrid)
+
+tests('SE', 'O', 'X')
+tests('SW', 'O', 'X')
 
 @app.route('/tic-tac-toe', methods=['POST'])
 def evaluateArena():
@@ -45,7 +62,6 @@ def evaluateArena():
     'W': '-', 'C': '-', 'E': '-', 
     'SW': '-', 'S': '-', 'SE': '-', }
     myId = 'O'
-    print_grid(grid)
 
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
@@ -54,13 +70,17 @@ def evaluateArena():
     URLplay = "https://cis2021-arena.herokuapp.com/tic-tac-toe/play/" + battleId 
     print(URLstart)
     messages = SSEClient(URLstart)
+
+    # to check initialized grid
+    print_grid(grid)
+
     for rawmsg in messages:
         msg = json.loads(rawmsg.__str__())
         print(msg)
         if 'youAre' in msg:
             myId = msg['youAre']
             print(myId)
-            if (myId != 'O'):
+            if (myId == 'X'):
                 if not make_post(payload, URLplay):
                     print("init failed")
                     return end_game
@@ -70,16 +90,17 @@ def evaluateArena():
         elif not 'action' in msg:
             make_post(invalid, URLplay)
         else: # player in message
-            # they flipped me
+            # you flipped me
             if msg['action'] != 'putSymbol':
                 print("you flipped me")
                 return end_game
             loc = msg['position']
-            player = msg['player']
-            if is_valid(grid, loc, player):
-                grid[loc] = player
-                if not is_valid(grid, payload['position'], myId):
+            you = msg['player']
+            if is_valid(grid, loc):
+                grid[loc] = you
+                if not is_valid(grid, payload['position']):
                     modify_position(payload, grid)
+                print("i play:", payload)
                 grid[payload['position']] = myId
                 make_post(payload, URLplay)
             else:
