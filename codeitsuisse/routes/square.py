ff59bf92883c11eb0343e4978b7c2d3923906d2b
trxe <peixianwong@gmail.com>
2021-09-25 09:56:53 +0800

    remove stonks temp

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -8,7 +8,7 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/square', methods=['POST'])
-def evaluateSquare():
+def evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
