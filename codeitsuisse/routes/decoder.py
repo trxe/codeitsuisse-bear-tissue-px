51d12f4c1c0d779189e570bff6b8dfc849d67b1d
trxe <peixianwong@gmail.com>
2021-09-26 04:20:49 +0800

    d

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -18,7 +18,7 @@ def evaluateDecoder():
     print(num_slots)
     for h in history:
         print(h)
-    guess = [0, 0, 0, 1, 1]
+    guess = [3, 3, 3, 1, 1]
     guess = [values[i] for i in guess]
     #guess = ['s', 'x', 's', 'b', 'b']
 
