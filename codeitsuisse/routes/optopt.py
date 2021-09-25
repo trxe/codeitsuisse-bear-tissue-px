8fb8e60a7de857b6dab3d692c3a6ce72bc74a75b
trxe <peixianwong@gmail.com>
2021-09-26 04:08:49 +0800

    fix optopt name

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -8,10 +8,10 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/optopt', methods=['POST'])
-def evaluate():
+def evaluateOptOpt():
     data = request.get_json()
     print(data)
-    # logging.info("data sent for evaluation {}".format(data))
+    logging.info("data sent for evaluation {}".format(data))
     return json.dumps(None)
 
 
