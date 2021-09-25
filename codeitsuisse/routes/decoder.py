1992a26604b6b8426930ff8ccbe52636cb88a997
trxe <peixianwong@gmail.com>
2021-09-25 21:25:03 +0800

    new guess decoder

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -17,7 +17,7 @@ def evaluateDecoder():
     print(values)
     print(num_slots)
     print(history)
-    guess = values[0:num_slots]
+    guess = values[1:num_slots+1]
 
     logging.info("My guess :{}".format(guess))
     return json.dumps({"answer" : guess})
