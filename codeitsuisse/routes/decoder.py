0638026fa07985cd4573597f7343a0b4b6b6afd3
trxe <peixianwong@gmail.com>
2021-09-26 01:08:40 +0800

    d

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -20,7 +20,7 @@ def evaluateDecoder():
         print(h)
     guess = [1, 1, 1, 0, 0]
     guess = [values[i] for i in guess]
-    guess = ['v', 'n', 'p', 'd', 'p']
+    guess = ['v', 'n', 'o', 'd', 'p']
 
     logging.info("My guess :{}".format(guess))
     return json.dumps({"answer" : guess})
